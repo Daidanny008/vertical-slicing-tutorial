4b7b9e86d7bbee4b272e7836ee2933aa5d072975
Daidanny008 <59802789+Daidanny008@users.noreply.github.com>
2025-01-17 11:29:33 -0500

    added quebec tax rate and item count (close #3)

--- tutorial2.py
+++ tutorial2.py
@@ -2,15 +2,29 @@ import argparse
 def main():
     # parse arguments
     parser = argparse.ArgumentParser()
+    parser.add_argument("count", type=float, help = "The number of the item")
     parser.add_argument("price", type=float, help = "The price of the item")
+    parser.add_argument(
+        "--province", 
+        type=str, 
+        default = "Ontario", 
+        choices=["Ontario", "Quebec"], 
+        help = "The province where the tax is applied (default ON)"
+        )
     args = parser.parse_args()
+    count = args.count
     price = args.price
 
+    if args.province == "Ontario":
+        tax_rate = 1.13
+    elif args.province == "Quebec":
+        tax_rate = 1.14975
+
     # apply ON tax
-    total = price * 1.13
+    total = count * price * tax_rate
 
     # print tax
-    print(f"The total price is {total:.2f}")
+    print(f"The total price in {args.province} is {total:.2f}")
 
 if __name__ == '__main__':
     main()
